839fbd1a0fe165d2470cec8d56deff9b77e252d7
Diego Macario <diego_macario@hotmail.com>
2024-06-08 07:14:17 -0300

    Retirada do método de teste não utilizado

--- src/test/java/br/com/contas/ContasApplicationTests.java
+++ src/test/java/br/com/contas/ContasApplicationTests.java
@@ -1,13 +1,9 @@
 package br.com.contas;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ContasApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
