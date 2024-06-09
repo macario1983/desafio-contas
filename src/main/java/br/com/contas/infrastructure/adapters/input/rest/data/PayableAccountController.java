8bc836e36f881495a4fb313b4372cbd8d2709513
Diego Macario <diego_macario@hotmail.com>
2024-06-09 20:37:35 -0300

    Ajuste no controller

--- src/main/java/br/com/contas/infrastructure/adapters/input/rest/data/PayableAccountController.java
+++ src/main/java/br/com/contas/infrastructure/adapters/input/rest/data/PayableAccountController.java
@@ -9,11 +9,15 @@ import br.com.contas.infrastructure.adapters.output.persitence.mapper.PayableAcc
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -57,31 +61,53 @@ public class PayableAccountController {
         this.logger.info("Terminando a requisição na camada REST para atualizar o payable account: {}", payableAccountRequest);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(payableAccountSaved);
-
     }
 
     @PatchMapping("/{id}/status")
-    public void updatePayableAccountStatus(@PathVariable UUID id) {
+    public ResponseEntity<PayableAccount> updatePayableAccountStatus(@PathVariable UUID id, @RequestBody Map<String, Object> fields) {
 
+        this.logger.info("Iniciando a requisição na camada REST para atualizar o status da conta a pagar com o ID: {}", id);
+        PayableAccount payableAccountSaved = this.updatePayableAccountUseCase.updatePayableAccountStatus(id, fields);
+        this.logger.info("Requisição na camada REST para atualizar o status da conta a pagar com o ID {} concluída", id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(payableAccountSaved);
     }
 
     @GetMapping("/{id}")
-    public void getPayableAccount(@PathVariable UUID id) {
+    public ResponseEntity<PayableAccount> getPayableAccountById(@PathVariable UUID id) {
 
+        this.logger.info("Iniciando a requisição na camada REST para obter a conta a pagar com o ID: {}", id);
+        PayableAccount payableAccountFound = this.getPayableAccountUseCase.getPayableAccountById(id);
+        this.logger.info("Requisição na camada REST para obter a conta a pagar com o ID {} concluída", id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(payableAccountFound);
     }
 
     @GetMapping
-    public void getPayableAccounts(@RequestParam(defaultValue = "0") int page,
-                                   @RequestParam(defaultValue = "10") int size,
-                                   @RequestParam(required = false) LocalDate dueDate,
-                                   @RequestParam(required = false) String description) {
+    public ResponseEntity<PageImpl<PayableAccount>> getPayableAccountsFiltered(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, @RequestParam(required = false) LocalDate dueDate, @RequestParam(required = false) String description) {
 
+        this.logger.info("Iniciando a requisição na camada REST para obter as contas a pagar. Parâmetros: dueDate={}, description={}", dueDate, description);
+        PageImpl<PayableAccount> payableAccountsPageountFound = this.getPayableAccountUseCase.getPayableAccountsFiltered(page, size, dueDate, description);
+        this.logger.info("Requisição na camada REST para obter as contas a pagar concluída");
+        return ResponseEntity.status(HttpStatus.OK).body(payableAccountsPageountFound);
     }
 
     @GetMapping("/total-paid")
-    public void getPayableAccountsAmountByPeriod(@RequestParam LocalDate startDate,
-                                                 @RequestParam LocalDate endDate) {
+    public ResponseEntity<BigDecimal> getPayableAccountsAmountByPeriod(@RequestParam LocalDate startDate, @RequestParam LocalDate endDate) {
 
+        this.logger.info("Iniciando a requisição na camada REST para obter o total pago das contas a pagar por período. Parâmetros: startDate={}, endDate={}", startDate, endDate);
+        BigDecimal amount = this.getPayableAccountUseCase.getPayableAccountsAmountByPeriod(startDate, endDate);
+        this.logger.info("Requisição na camada REST para obter o total pago das contas a pagar por período concluída");
+        return ResponseEntity.status(HttpStatus.OK).body(amount);
     }
 
+    @PostMapping("/import")
+    public ResponseEntity<String> importCSV(@RequestParam("file") MultipartFile file) {
+        try {
+            this.createPayableAccountUseCase.importCSV(file);
+            return ResponseEntity.ok("Arquivo importado com successo");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error ao importar o arquivo: " + e.getMessage());
+        }
+    }
 }
